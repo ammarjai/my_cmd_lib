22adbd088230c58c3ec19205d9470ec971e5fd6b
Ahmad Ammar Asyraaf Jainuddin <ammarjainuddin@gmail.com>
2020-07-21 12:50:42 +0800

    Added my_conf and add copy conf to home into fresh_linux.sh

--- fresh_linux.sh
+++ fresh_linux.sh
@@ -17,15 +17,21 @@ sudo apt-get install -y curl python3.7 python3-setuptools libpython3.7
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.7 get-pip.py
 
+# Set alias for python3.7
 echo '' >> .bashrc
 echo 'alias python=python3.7' >> .bashrc
 echo 'alias pip=pip3.7' >> .bashrc
 source .bashrc
 source .profile
 
+# Cleanup
 rm get-pip.py
 
+# Setup python virtualenv
 pip install virtualenv
 virtualenv -p python3.7 venv
 echo 'source ~/venv/bin/activate' >> .bashrc
 source .bashrc
+
+# Copy my configuration to home
+cp ./my_conf/* ~/
---
+++ my_conf/.vimrc
@@ -0,0 +1,8 @@
+set number
+set rnu
+syntax on
+
+filetype plugin indent on
+set tabstop=4
+set shiftwidth=4
+set expandtab
